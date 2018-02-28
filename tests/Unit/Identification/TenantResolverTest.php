06749e754bd6b577fe2ed8371260d4ed2e377819
luceos <daniel+github@klabbers.email>
2018-02-28 15:26:52 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/Identification/TenantResolverTest.php
+++ tests/Unit/Identification/TenantResolverTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
 namespace Tenancy\Tests\Unit\Identification;
 
 use Illuminate\Contracts\Events\Dispatcher;
@@ -34,7 +46,7 @@ class TenantResolverTest extends TestCase
     /**
      * @test
      */
-    function returns_tenant_when_resolved()
+    public function returns_tenant_when_resolved()
     {
         $this->events->listen(Resolving::class, function ($event) {
             return $this->tenant;
