ce710cdafc84eb88967b569361de643aea3c717e
luceos <daniel+github@klabbers.email>
2018-03-12 06:41:14 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Eloquent/ConnectionResolver.php
+++ src/Eloquent/ConnectionResolver.php
@@ -61,7 +61,6 @@ class ConnectionResolver implements ConnectionResolverInterface
             $tenant = $this->environment->getTenant() &&
             config("database.connections.$name.uuid") !== $tenant->getTenantKey() &&
             $provider = $this->manager->__invoke($tenant, $name)) {
-
             $configuration = $provider->configure($tenant);
 
             Arr::set($configuration, 'uuid', $tenant->getTenantKey());
